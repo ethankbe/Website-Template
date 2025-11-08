b3d35e48c048175ad9efae44eb919882e0bf6361
Ethan Kurt Espejo <ebespejo@up.edu.ph>
2025-11-08 09:50:54 +0800

    ne

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   publicDir: "./static",
-  base: "/Website-Template/",
+  base: "./",
   css: {
     postcss: {
       plugins: [tailwind()],
